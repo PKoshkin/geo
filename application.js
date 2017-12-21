aa8ade6a11320cbf9421fa829321c15889f8505e
Pavel Koshkin <koshkin.pasha@gmail.com>
2017-12-21 18:10:11 +0300

    Started rewriting. Will be guided by UML classes diagram. Initing Point and Application classes.

---
+++ application.js
@@ -0,0 +1,13 @@
+'use strict'
+
+class Application {
+    constructor() {
+        let width = window.innerWidth; // Получаем ширину экрана
+        let height = window.innerHeight; // Получаем высоту экрана
+        PIXI.settings.RENDER_OPTIONS.backgroundColor = 0xFFFFFF;
+        PIXI.settings.RENDER_OPTIONS.antialias = true;
+        this.application = new PIXI.Application(width, height); // Создаем холст
+        document.body.appendChild(application.view); // Выводим его в тело страницы
+    }
+}
+
